fe944408aff4b967f8997cd2df1f96abfe471216
Gabe Finch <gabefinch@gmail.com>
2015-01-23 09:55:05 -0800

    first commit. add Gemfile and create_database.rb

---
+++ create_database.rb
@@ -0,0 +1,12 @@
+require('pg')
+GUEST_DB = PG.connect({:dbname => "Guest"})
+
+GUEST_DB.exec("CREATE DATABASE hair_salon;")
+SALON_DB = PG.connect({:dbname => "hair_salon"})
+SALON_DB.exec("CREATE TABLE stylists (id serial PRIMARY KEY, name varchar);")
+SALON_DB.exec("CREATE TABLE clients (id serial PRIMARY KEY, name varchar);")
+SALON_DB.exec("CREATE DATABASE hair_salon_test WITH TEMPLATE hair_salon;")
+
+
+# DB.exec("DROP DATABASE hair_salon;")
+# DB.exec("DROP DATABASE hair_salon_test;")
